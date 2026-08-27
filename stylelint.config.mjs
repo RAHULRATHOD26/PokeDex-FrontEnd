c86798e1fefaffee65c7015a23e382b8b117538a
RAHULRATHOD26 <0rahulrathod07@gmail.com>
2026-08-27 21:55:59 +0530

    Improve CI validation

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: ["stylelint-config-standard"],
+    rules: {
+        "selector-id-pattern": null,
+        "no-descending-specificity": null
+    }
+};
